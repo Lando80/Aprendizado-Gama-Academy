c21a936fb2564263289c43ce02aea1924bd40a1f
Lando80 <lando.s@hotmail.com>
2021-07-25 12:32:20 -0400

    atualização - aula 08

--- 10 GraphQL/client-server-example/packages/server/src/main.js
+++ 10 GraphQL/client-server-example/packages/server/src/main.js
@@ -17,38 +17,6 @@ const server = createServer((request, response) => {
             break;
         }
 
-        case '/sign-in': {
-            const path = resolve(__dirname,'./pages/sign-in.html');
-            readFile(path, (error, file) =>{
-                if(error) {
-                    response.writeHead(500, 'Can\'t process HTML file.');
-                    response.end();
-                    return;
-                }
-
-                response.writeHead(200);
-                response.write(file);
-                response.end();
-            })
-            break;
-        }
-            
-        case '/home': {
-            const path = resolve(__dirname,'./pages/home.html');
-            readFile(path, (error, file) =>{
-                if(error) {
-                    response.writeHead(500, 'Can\'t process HTML file.');
-                    response.end();
-                    return;
-                }
-
-                response.writeHead(200);
-                response.write(file);
-                response.end();
-            })
-            break;
-        }
-
         case '/authenticate':{
             let data = '';
             request.on('data', (chunk) =>{
@@ -57,9 +25,7 @@ const server = createServer((request, response) => {
             request.on('end', () => {
                 const params = parse(data);
                 console.log(parse(data));
-                response.writeHead(301, {
-                    Location: '/home',
-                });
+                
                 response.end();
             });
             break;
